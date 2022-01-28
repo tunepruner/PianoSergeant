0f872b237c5342ba57df6b8183b97fd44e8dca45
Phil Carlson <writephilcarlson@gmail.com>
2022-01-27 20:23:09 -0500

    viewModel now initializes values reactively

--- app/src/main/java/com/tunepruner/musictraining/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/com/tunepruner/musictraining/viewmodel/SettingsViewModel.kt
@@ -3,10 +3,13 @@ package com.tunepruner.musictraining.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tunepruner.musictraining.repositories.Mode
 import com.tunepruner.musictraining.repositories.SettingsRepository
 import com.tunepruner.musictraining.repositories.TimeConstraint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 class SettingsViewModel(private val settingsRepo: SettingsRepository) : ViewModel() {
@@ -19,8 +22,12 @@ class SettingsViewModel(private val settingsRepo: SettingsRepository) : ViewMode
     var timeConstraint: LiveData<TimeConstraint> = _timeConstraint
 
     init {
-        _mode.value = settings.mode
-        _timeConstraint.value = settings.timeConstraint
+        viewModelScope.launch {
+            settingsRepo.current.collect {
+                _mode.value = it.mode
+                _timeConstraint.value = it.timeConstraint
+            }
+        }
     }
 
     fun enableChordMode() {
